0b318b3c43cc022d0e2da1b9a927b5c323b204f6
Flor Martinez <flor@MacBook-Air-de-Flor.local>
2017-03-27 20:57:05 -0600

    agregar cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/batman'
 
+  get 'pages/superman'
+
+  root 'pages#batman_vs_superman'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
